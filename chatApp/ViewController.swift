d101c0c2ea2ad4c49cbd186db41e1f8c278e1e92
github user yuta-2525 <Yuta1091.3150@gmail.com>
2021-11-13 17:38:48 +0900

    Initial Commit

---
+++ chatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chatApp
+//
+//  Created by 佐藤勇太 on 2021/11/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
